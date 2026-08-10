a31b203884bf7a29eeda75ff237100d5cb94046b
Weller91 <weller91@me.com>
2026-08-10 21:43:51 +0800

    Modernize UART.h

--- UART.h
+++ UART.h
@@ -17,17 +17,17 @@ class UART
 {
 public:
 	UART(uint8_t uart = 1);
-	
+
 	~UART();
-	
+
 	static void clear();
-	
+
 	bool begin(uint32_t baud = 9600, bool nine_bit = false);
 	void end();
 	int available();
 	int peek();
 	void flush();
-	
+
 	inline void print(const char c) { write((uint8_t)c); }
 	void print(const char* c);
 	void print(const String s);
@@ -37,7 +37,7 @@ public:
 	void print(const unsigned long l);
 	void print(const float f);
 	void print(const double d);
-	
+
 	inline UART& operator<<(const char c) { print(c); return *this; }
 	inline UART& operator<<(const char* c) { print(c); return *this; }
 	inline UART& operator<<(const String s) { print(s); return *this; }
@@ -47,7 +47,7 @@ public:
 	inline UART& operator<<(const unsigned long lu) { print(lu); return *this; }
 	inline UART& operator<<(const float f) { print(f); return *this; }
 	inline UART& operator<<(const double d) { print(d); return *this; }
-	
+
 	inline void println(const char c) { print(c); println(); }
 	inline void println(const char* c) { print(c); println(); }
 	inline void println(const String s) { print(s); println(); }
@@ -56,24 +56,25 @@ public:
 	inline void println(const long l) { print(l); println(); }
 	inline void println(const float f) { print(f); println(); }
 	inline void println(const double d) { print(d); println(); }
-	
+
 	inline void println() { print(endl); }
-	
+
 	size_t write(uint8_t data);
 	inline size_t write(int data) { return write((uint8_t)data); }
 	size_t write9bit(uint16_t data);
-	
+
 	int read();
 	bool readUL(unsigned long *val);
 
 	bool error();
 	bool ninthBitSet();
 	inline uint8_t getTXPin() { return m_TXn; }
-	
+
 private:
 	uint8_t m_TXn;
 	uint8_t m_uart;
-	
+	bool m_started;
+
 	uint8_t RXENn;
 	uint8_t TXENn;
 	uint8_t RXCIEn;
@@ -82,7 +83,7 @@ private:
 	uint8_t UCSZn2;
 	uint8_t USBSn;
 	uint8_t U2Xn;
-	
+
 	volatile uint8_t *v_UBRRnH;
 	volatile uint8_t *v_UBRRnL;
 	volatile uint8_t *v_UCSRnC;
